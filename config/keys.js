ff7f0dc2ec4bc1ba45e3e8df97d78acd89721c29
AlMag1 <alex.aris90@gmail.com>
2019-03-21 23:39:22 +0200

    finished user api

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://admin:administrator13@ds227939.mlab.com:27939/devconnect"
+  mongoURI:
+    "mongodb://admin:administrator13@ds227939.mlab.com:27939/devconnect",
+  secretOrKey: "secret"
 };
