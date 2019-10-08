1e4cb7a8b9faa234af5b81d62398dcd74ecca02a
Aris Alexopoulos <aris@aegeantaxi.com>
2019-10-08 21:35:41 +0300

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import store from './store';
 
 class App extends Component {
     render() {
+        console.log('test');
         return (
             <Provider store={store}>
                 <Router>
