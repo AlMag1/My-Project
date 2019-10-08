c74ee69c25f4ecf182e777924a28d5dce06ba786
Aris Alexopoulos <aris@aegeantaxi.com>
2019-10-08 11:57:14 +0300

    content changes

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default () => {
-  return (
-    <footer className="footer.bg-dark.text-white.mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevConnector
-    </footer>
-  );
+    return (
+        <footer className="footer.bg-dark.text-white.mt-5 p-4 text-center">
+            Copyright &copy; {new Date().getFullYear()} Imagination
+        </footer>
+    );
 };
